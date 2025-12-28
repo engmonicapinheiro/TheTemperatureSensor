2f8c08ec6ff9509f75a3ccb5627d107bd175b29c
Alexandre Martins <alexmsmartins@gmail.com>
2025-12-28 23:01:20 +0000

    Add blinking LED behaviour.
    Details:
    - define the initialization routines including
      - initializing the proper pin for the LED
      - setting the right modes
    - define a software interrupt handler that helps HAL_Delay work
      properly
    - loop that turns the LED on and off every 1 sec

--- src/main.c
+++ src/main.c
@@ -1,4 +1,70 @@
-int main()
-{
-  return 0;
+#include "stm32g4xx_hal.h"
+
+void SystemClock_Config(void);
+
+static void MX_GPIO_Init(void);
+
+int main() {
+    HAL_Init();
+    SystemClock_Config();
+    MX_GPIO_Init();
+
+    while (1) {
+        HAL_GPIO_WritePin(GPIOB, GPIO_PIN_8, GPIO_PIN_RESET);
+        HAL_Delay(1000);
+        HAL_GPIO_WritePin(GPIOB, GPIO_PIN_8, GPIO_PIN_SET);
+        HAL_Delay(1000);
+    }
+}
+
+void SystemClock_Config(void) {
+    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+    HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+    RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+    HAL_RCC_OscConfig(&RCC_OscInitStruct);
+
+    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
+                                  | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+    HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0);
+}
+
+static void MX_GPIO_Init(void) {
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+
+    GPIO_InitStruct.Pin = GPIO_PIN_8;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_8, GPIO_PIN_SET);
+}
+
+/**
+ * @brief Handler for the SysTick interrupt.
+ *
+ * This function is invoked whenever the SysTick interrupt occurs. It increments
+ * the system tick counter by calling the HAL_IncTick() function, which is used
+ * for timekeeping operations within the system.
+ *
+ * The SysTick interrupt is typically used for periodic tasks and time-based
+ * operations in embedded systems.
+ *
+ * In this case, the counter is being used to define when HAL_Delay should stop
+ * delaying execution of the next line of code.
+ */
+void SysTick_Handler(void) {
+    HAL_IncTick();
 }
